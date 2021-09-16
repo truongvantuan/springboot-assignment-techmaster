10e0c97574d00da707521e33815c3d4ae9a503d7
Trương Tuấn <truongvantuan@outlook.com.vn>
2021-09-16 16:06:16 +0700

    create branch spring-jpa-h2

--- springjpa-h2/src/main/java/com/company/h2jpa/repository/CustomerRepository.java
+++ springjpa-h2/src/main/java/com/company/h2jpa/repository/CustomerRepository.java
@@ -3,9 +3,5 @@ package com.company.h2jpa.repository;
 import com.company.h2jpa.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-    List<Customer> findAllById(int id);
 }
